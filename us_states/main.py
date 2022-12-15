b50474703f3423e2955658e49715babe67d53669
egvidal <egvidal@yahoo.com.ar>
2022-12-15 16:49:21 -0300

    update

--- us_states/main.py
+++ us_states/main.py
@@ -25,6 +25,13 @@ counter.write(arg=f"{len(found_states)}/50", font=("Arial", 30, "bold"))
 while len(found_states) < 50:
   player_input = screen.textinput("US States", "Enter the name of the State").title()
   if player_input == "Exit":
+    missing_states = [state for state in all_states if state not in found_states]
+    # print(missing_states)
+    my_dict = {
+      "Missing States": missing_states  
+    }
+    data_frame = pandas.DataFrame(missing_states)
+    data_frame.to_csv("./missing_states.csv")
     break
   elif player_input in all_states and player_input not in found_states:
     label.goto(int(states[states.state == player_input].x), int(states[states.state == player_input].y))
@@ -33,11 +40,3 @@ while len(found_states) < 50:
     counter.clear()
     counter.goto(-360, 210)
     counter.write(arg=f"{len(found_states)}/50", font=("Arial", 30, "bold"))
-
-missing_states = [state for state in all_states if state not in found_states]
-print(missing_states)
-my_dict = {
-  "Missing States": missing_states
-}
-data_frame = pandas.DataFrame(my_dict)
-data_frame.to_csv("./missing_states.csv")
